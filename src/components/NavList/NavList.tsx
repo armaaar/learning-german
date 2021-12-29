95ad881064759baeab3b3b6a7681136130e646c1
Ahmed Rafik Ibrahim <ahmedrafik_maaar@outlook.com>
2021-12-29 11:29:51 +0100

    feat: Create NavList component

---
+++ src/components/NavList/NavList.tsx
@@ -0,0 +1,21 @@
+import { Component, For } from "solid-js";
+import styles from "./NavList.module.scss";
+
+type Props = {
+  list: string[];
+  onSelect: (item: string) => void;
+};
+
+export const NavList: Component<Props> = (props) => (
+  <nav class={styles.nav}>
+    <ul class={styles.list}>
+      <For each={props.list}>
+        {(item) => (
+          <li class={styles.item} onClick={() => props.onSelect(item)}>
+            {item}
+          </li>
+        )}
+      </For>
+    </ul>
+  </nav>
+);
